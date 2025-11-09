0724f67c32ff2a972176f1746460073a7a6cd11f
Ana Maria Henao <amariahmesa@gmail.com>
2025-11-09 18:18:57 -0500

    Rename UML file and add image asset
    Renamed ClasesProyecto.puml to ClasesProyecto.plantuml for consistency and added image.png to UML resources. AssemblyInfo files updated due to build changes.

--- obj/Debug/net8.0-windows/ProyectoEstupido.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/ProyectoEstupido.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoEstupido")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f96059db78f3cca80edd170c2c70b78da088205")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a57b0edb1af1f1e4063b74c32c39df6450eb4dc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoEstupido")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoEstupido")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
